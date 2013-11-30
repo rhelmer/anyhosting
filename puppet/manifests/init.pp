51a6fb0f3dfb64e87a7e562b1359873b7d656c2c
Robert Helmer <rhelmer@rhelmer.org>
2013-11-29 22:40:49 -0800

    install docker.io

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,6 +1,6 @@
 Exec {
     user => "vagrant",
-    logoutput => on_failure
+    logoutput => on_failure,
 }
 
 class anyhosting {
@@ -11,25 +11,47 @@ class anyhosting {
         recurse => true,
         source => "/vagrant/puppet/files/etc/nginx",
         ensure => directory,
-        require => Package["nginx"]
+        require => Package["nginx"];
     }
 }
 
 exec {
-    "/usr/bin/apt-get update":
-        alias => "apt-get-update",
+    "apt-get-update":
+        command => "/usr/bin/apt-get update",
         cwd => "/root",
-        user => "root"
+        user => "root";
+
+    "add-docker-key":
+        command => "/usr/bin/curl https://get.docker.io/gpg | apt-key add -",
+        cwd => "/root",
+        user => "root";
+
+    "add-docker-repo":
+        command => '/usr/bin/apt-add-repository "deb http://get.docker.io/ubuntu docker main"',
+        require => Exec["add-docker-key"],
+        cwd => "/root",
+        user => "root";
+
+    "apt-get-update-docker-repo":
+        command => "/usr/bin/apt-get update",
+        require => Exec["add-docker-repo"],
+        cwd => "/root",
+        user => "root";
 }
 
 package {
-    "nginx":
+    ["nginx", "linux-image-extra-3.8.0-19-generic"]:
         ensure => latest,
-        require => Exec["apt-get-update"]
+        require => Exec["apt-get-update"];
+
+    "lxc-docker":
+        ensure => latest,
+        require => Exec["apt-get-update-docker-repo"];
 }
 
-group { "puppet":
-    ensure => "present"
+group {
+    "puppet":
+        ensure => "present";
 }
 
 include anyhosting
