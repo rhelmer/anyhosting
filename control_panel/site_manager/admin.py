4189e5c47f82ec86728cf4f9f3147962947b025a
Robert Helmer <rhelmer@rhelmer.org>
2013-11-29 17:11:23 -0800

    upgrade django

--- control_panel/site_manager/admin.py
+++
@@ -1,6 +0,0 @@
-from control_panel.site_manager.models import User, Domain, Website
-from django.contrib import admin
-
-admin.site.register(User)
-admin.site.register(Domain)
-admin.site.register(Website)
