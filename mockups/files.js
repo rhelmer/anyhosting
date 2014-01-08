b177a435bd3b9bc47686af63cf52e29182cfdc89
Robert Helmer <rhelmer@rhelmer.org>
2014-01-08 02:49:10 -0800

    fade background, and tweak close button

--- mockups/files.js
+++ mockups/files.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
     $('#upload-button').click(function() {
+        $('html,body').css('background-color', 'grey');
         $('#newfolder').hide();
         $('#upload').fadeToggle();
     });
     $('#newfolder-button').click(function() {
+        $('html,body').css('background-color', 'grey');
         $('#upload').hide();
         $('#newfolder').fadeToggle();
     });
     $('.close-button').click(function() {
+        $('html,body').css('background-color', '#8ab5a2');
         $('#upload').fadeOut();
         $('#newfolder').fadeOut();
     })
     $('#upload-ok').click(function(event) {
+        $('html,body').css('background-color', '#8ab5a2');
         event.preventDefault();
         $('#upload').fadeOut();
     });
     $('#newfolder-ok').click(function(event) {
+        $('html,body').css('background-color', '#8ab5a2');
         event.preventDefault();
         $('#newfolder').fadeOut();
     });
